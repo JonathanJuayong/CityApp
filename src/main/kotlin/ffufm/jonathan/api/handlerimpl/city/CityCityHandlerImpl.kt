db5d65f51a1e68c3acef1f7af43aefbf0a540c98
Jonathan Juayong <jonathan.juayong@gmail.com>
2022-01-26 15:39:45 +0800

    implement code CRUD methods

--- src/main/kotlin/ffufm/jonathan/api/handlerimpl/city/CityCityHandlerImpl.kt
+++ src/main/kotlin/ffufm/jonathan/api/handlerimpl/city/CityCityHandlerImpl.kt
@@ -1,28 +1,41 @@
 package ffufm.jonathan.api.handlerimpl.city
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import de.ffuf.pass.common.handlers.PassDatabaseHandler
 import de.ffuf.pass.common.utilities.extensions.orElseThrow404
+import de.ffuf.pass.common.utilities.extensions.toDtos
+import ffufm.jonathan.api.external.WeatherService
 import ffufm.jonathan.api.repositories.city.CityCityRepository
 import ffufm.jonathan.api.spec.dbo.city.CityCity
 import ffufm.jonathan.api.spec.dbo.city.CityCityDTO
 import ffufm.jonathan.api.spec.handler.city.CityCityDatabaseHandler
-import kotlin.Int
-import kotlin.Long
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.findByIdOrNull
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Component
 
 @Component("city.CityCityHandler")
-class CityCityHandlerImpl : PassDatabaseHandler<CityCity, CityCityRepository>(),
-        CityCityDatabaseHandler {
+class CityCityHandlerImpl(
+    private val weatherService: WeatherService,
+    private val objectMapper: ObjectMapper
+) : PassDatabaseHandler<CityCity, CityCityRepository>(),
+    CityCityDatabaseHandler {
     /**
      * Create City: Creates a new City object
      * HTTP Code 201: The created City
      */
     override suspend fun create(body: CityCityDTO): CityCityDTO {
-        TODO("not checked yet")
-        return repository.save(body)
+        val weather = weatherService.getWeather(body.name ?: "")
+
+        val weatherJsonString = objectMapper.writeValueAsString(weather)
+        val jsonNode = objectMapper.readTree(weatherJsonString)
+        val forecasts = jsonNode.get("forecast").asIterable()
+        val day1Temperature = forecasts.first().get("temperature").asText()
+
+        return repository.save(
+            body.copy(
+                temperature = day1Temperature
+            ).toEntity()
+        ).toDto()
     }
 
     /**
@@ -30,9 +43,9 @@ class CityCityHandlerImpl : PassDatabaseHandler<CityCity, CityCityRepository>(),
      * Headers will include TotalElements, TotalPages, CurrentPage and PerPage to help with Pagination.
      * HTTP Code 200: List of Cities
      */
-    override suspend fun getAll(maxResults: Int = 100, page: Int = 0): Page<CityCityDTO> {
-        TODO("not checked yet")
-        return repository.findAll(Pageable.unpaged())
+    override suspend fun getAll(maxResults: Int, page: Int): Page<CityCityDTO> {
+        val pagination = PageRequest.of(page, maxResults)
+        return repository.findAll(pagination).toDtos()
     }
 
     /**
@@ -40,7 +53,6 @@ class CityCityHandlerImpl : PassDatabaseHandler<CityCity, CityCityRepository>(),
      */
     override suspend fun remove(id: Long) {
         val original = repository.findById(id).orElseThrow404(id)
-        TODO("not checked yet - update the values you really want updated")
         return repository.delete(original)
     }
 
@@ -52,7 +64,12 @@ class CityCityHandlerImpl : PassDatabaseHandler<CityCity, CityCityRepository>(),
      */
     override suspend fun update(body: CityCityDTO, id: Long): CityCityDTO {
         val original = repository.findById(id).orElseThrow404(id)
-        TODO("not checked yet - update the values you really want updated")
-        return repository.save(original)
+        return repository.save(
+            original.copy(
+                name = body.name ?: original.name,
+                country = body.country ?: original.country,
+                temperature = body.temperature ?: original.temperature
+            )
+        ).toDto()
     }
 }
