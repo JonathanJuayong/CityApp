3c3ce6b15e46ff02687d96e9fb4d7be00519b4cd
Jonathan Juayong <jonathan.juayong@gmail.com>
2022-01-26 15:28:07 +0800

    create external service for third party api

---
+++ src/main/kotlin/ffufm/jonathan/api/external/WeatherService.kt
@@ -0,0 +1,33 @@
+package ffufm.jonathan.api.external
+
+import ffufm.jonathan.api.utils.Constants
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+
+@Service
+class WeatherService {
+    fun initializeApiClient(url: String) =
+        WebClient.builder()
+            .baseUrl(url)
+            .filter { request, next -> next.exchange(request)}
+            .build()
+
+
+    suspend fun getTemperature(cityName: String): String {
+        val client = initializeApiClient(Constants.WEATHER_URL)
+        return client.get()
+            .uri { builder ->
+                builder
+                    .apply {
+                        path("/$cityName")
+                    }
+                    .build()
+            }
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            .retrieve()
+            .awaitBody()
+    }
+}
